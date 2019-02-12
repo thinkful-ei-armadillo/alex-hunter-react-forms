e3dee26069d7ab8eceeeed19dfc6ed9409e5471d
AReach <DerAReich@gmail.com>
2019-02-12 09:58:04 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,22 @@
-import React from 'react';
+import React from "react";
+import Tabs from './state/Tabs';
 
-function App (props) {
-  return (
-      <div>
-          <header>
-              <img src="https://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg" alt="Sally Smith"/>
-              <h1>Sally Smith</h1>
-              <hr/>
-              <address>
-                  <span>1234 South 5th Street -</span>
-                  <span>123-456-7890 -</span>
-                  <span>sally.smith@fakeemail.com </span>
-              </address>
-              <hr/>
-          </header>
-          <main>
-              <section>
-                  <h2>Education</h2>
-                  <h3>Snow Day University</h3>
-                  <p>Bachelors in Snowball Engineering</p>
-                  <ul>
-                      <li>Valedictorian.</li>
-                      <li>4.00 GPA.</li>
-                      <li>Vice-Chair of the Basket Weaving club.</li>
-                  </ul>
-                  <hr/>
-              </section>
-              <section>
-                  <h2>Snowball Emporium</h2>
-                  <h3>Senior Snowball Developer</h3>
-                  <p>8/3/2016 - Present</p>
-                  <ul>
-                      <li>Worked on the team that developed the Snowballenator 3.0.</li>
-                      <li>Managed ten different teams working on 30 different projects.</li>
-                  </ul>
-                  <br/>
-                  <h2>Rudolph's Construction</h2>
-                  <h3>Junior Snowball Engineer</h3>
-                  <p>2/4/2014 - 7/3/2016</p>
-                  <ul>
-                      <li>Youngest Junior Engineer to design their own snowball.</li>
-                      <li>Helped improve construction methodology to increase throughput 500%!</li>
-                  </ul>
-              </section>
-          </main>
-      </div>
-  );
+// array of objects each with a name and content
+const tabsProp = [
+    { name: 'First tab',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+    { name: 'Second tab',
+        content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+    { name: 'Third tab',
+        content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+
+class App extends React.Component {
+  render() {
+    return <div>
+      <Tabs tabs={tabsProp} />
+    </div>
+  }
 }
 
 export default App;
